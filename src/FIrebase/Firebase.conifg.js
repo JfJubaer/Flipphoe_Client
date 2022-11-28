6cf4697369cbd096687d9064e10224b295730a3c
JfJubaer <skk5727@gmail.com>
2022-11-29 00:41:59 +0600

    11th

---
+++ src/FIrebase/Firebase.conifg.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCKSfDg_2cL2FHAhcJ_bqMeYPEtSBuLHIU",
+    authDomain: "flipphoe-8704e.firebaseapp.com",
+    projectId: "flipphoe-8704e",
+    storageBucket: "flipphoe-8704e.appspot.com",
+    messagingSenderId: "743238106500",
+    appId: "1:743238106500:web:764b70741f273c60ecb1d1"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
