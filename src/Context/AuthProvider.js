4779b3bee4ef9dff2c7fd69aeb98aab9a5545a6b
JfJubaer <skk5727@gmail.com>
2022-11-29 00:47:43 +0600

    12th

---
+++ src/Context/AuthProvider.js
@@ -0,0 +1,67 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth'
+import { app } from '../FIrebase/Firebase.conifg';
+
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const provider = new GoogleAuthProvider();
+
+
+
+
+    const providerLogin = () => {
+        setLoading(true);
+        return signInWithPopup(auth, provider);
+    }
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const updateUserProfile = (profile) => {
+        return updateProfile(auth.currentUser, profile);
+    }
+
+
+    const logOut = () => {
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+
+
+            setUser(currentUser);
+
+            setLoading(false);
+        });
+
+        return () => {
+            unsubscribe();
+        }
+
+    }, [])
+
+    const authInfo = {
+        user, loading, setLoading, providerLogin, logOut, updateUserProfile, createUser, signIn
+    };
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
